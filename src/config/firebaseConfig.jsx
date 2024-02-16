6cb408a40a4d48c675ab24078fd7d1ac059f7606
Bugra <seyitbugraerden@gmail.com>
2024-02-17 02:44:46 +0300

    Firebase

---
+++ src/config/firebaseConfig.jsx
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCr7br6RBHPsNwDHKMcctibPsdi9SyBMBw",
+  authDomain: "chat-app-db13b.firebaseapp.com",
+  projectId: "chat-app-db13b",
+  storageBucket: "chat-app-db13b.appspot.com",
+  messagingSenderId: "1010845666598",
+  appId: "1:1010845666598:web:a37b77490471392dbcc5e7",
+};
+
+const app = initializeApp(firebaseConfig);
