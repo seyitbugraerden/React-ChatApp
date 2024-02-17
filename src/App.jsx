02bd5f664a35ad4eb3cd5d18d98db3b6d42aa23d
Bugra <seyitbugraerden@gmail.com>
2024-02-17 03:19:01 +0300

    UseRef imported

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,39 @@
+import { useState, useRef } from "react";
 import Auth from "./components/Auth";
+import Cookies from "universal-cookie";
+
+const cookies = new Cookies();
+
 function App() {
+  const [isSignIn, setIsSignIn] = useState(cookies.get("auth-token")); // boolean
+  const [room, setRoom] = useState(null);
+
+  const roomInputRef = useRef(null);
+
   return (
     <>
-      <Auth />
+      {isSignIn ? (
+        room ? (
+          <div>Chat</div>
+        ) : (
+          <div className="room">
+            <input
+              type="text"
+              placeholder="Chat Room Name"
+              ref={roomInputRef}
+            />
+            <button
+              onClick={() => {
+                setRoom(roomInputRef.current.value);
+              }}
+            >
+              Enter Chat
+            </button>
+          </div>
+        )
+      ) : (
+        <Auth />
+      )}
     </>
   );
 }
--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -1,9 +1,17 @@
 import React from "react";
 import { auth, provider } from "./config/firebaseConfig";
 import { signInWithPopup } from "firebase/auth";
+import Cookies from "universal-cookie";
+
+const cookies = new Cookies();
 
 const signInWithGoogle = async () => {
-  await signInWithPopup(auth, provider);
+  try {
+    const result = await signInWithPopup(auth, provider);
+    cookies.set("auth-token", result.user.refreshToken);
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 function Auth() {
