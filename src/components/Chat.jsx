f84464b312fbab525cce32c4136ab75e5a842026
Bugra <seyitbugraerden@gmail.com>
2024-02-17 12:48:17 +0300

    Chat integration

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,11 +1,30 @@
-import React, { useState } from "react";
-import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
+import {
+  addDoc,
+  collection,
+  serverTimestamp,
+  onSnapshot,
+  query,
+  where,
+} from "firebase/firestore";
 import { auth, db } from "../components/config/firebaseConfig";
 
 function Chat({ room }) {
   const [newMessage, setNewMessage] = useState("");
+  const [messages, setMessages] = useState([]);
 
   const messagesRef = collection(db, "messages");
+
+  useEffect(() => {
+    const queryMessages = query(messagesRef, where("room", "==", room));
+    onSnapshot(queryMessages, (snapshot) => {
+      let messages = [];
+      snapshot.forEach((doc) => {
+        messages.push({ ...doc.data(), id: doc.id });
+      });
+      setMessages(messages);
+    });
+  }, []);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -18,8 +37,14 @@ function Chat({ room }) {
     });
     setNewMessage("");
   };
+
   return (
     <div className="chat-app">
+      {messages.map((item) => (
+        <h1 key={item.id}>
+          {item.text} -{item.user}
+        </h1>
+      ))}
       <form className="new-message-form" onSubmit={handleSubmit}>
         <input
           type="text"
