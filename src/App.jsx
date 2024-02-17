3b84669f5f2dfd65dfb1134d826361fe0320c3c9
Bugra <seyitbugraerden@gmail.com>
2024-02-17 22:23:07 +0300

    Buttons & Join Elements

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Auth from "./components/Auth";
 import Cookies from "universal-cookie";
 import Chat from "./components/Chat";
 import { Card } from "primereact/card";
+import { InputText } from "primereact/inputtext";
 import "./styles/app.css";
 const cookies = new Cookies();
 
@@ -19,18 +20,23 @@ function App() {
           <Chat room={room} />
         ) : (
           <div className="room">
-            <input
-              type="text"
-              placeholder="Chat Room Name"
-              ref={roomInputRef}
-            />
-            <button
-              onClick={() => {
-                setRoom(roomInputRef.current.value);
-              }}
-            >
-              Enter Chat
-            </button>
+            <h2>Enter Chat Room</h2>
+            <div className="buttonarea">
+              <InputText
+                type="text"
+                placeholder="Chat Room Name"
+                ref={roomInputRef}
+              />
+              <div
+                className="two-part-button"
+                onClick={() => {
+                  setRoom(roomInputRef.current.value);
+                }}
+              >
+                <div className="call-to-action">JOIN</div>
+                <div className="description">happy chatting !</div>
+              </div>
+            </div>
           </div>
         )
       ) : (
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { PrimeReactProvider } from "primereact/api";
-import "primereact/resources/themes/lara-light-cyan/theme.css";
+import "primereact/resources/themes/lara-light-purple/theme.css";
 import "primeicons/primeicons.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
